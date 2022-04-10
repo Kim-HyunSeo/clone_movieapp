1be85667f31c2f594d622d77d44c088ec9180289
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-04-11 05:24:26 +0900

    #6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState, useEffect } from "react";
+
 import Button from "./Button";
 import styled from "./App.module.css";
 
 function App() {
+    const [value, setValue] = useState(0);
+    const onClick = () => setValue((prev) => prev + 1);
+    console.log(`Set value to ${value}.`);
+
+    useEffect(() => {
+        console.log("Call the API...");
+    }, []);
+
     return (
         <div>
-            <h1 className={styled.title}>Welcome back!!!</h1>
+            <h1 className={styled.title}>{value}</h1>
+            <button onClick={onClick}>Click Me</button>
+            <br />
             <Button text={"Continue"} />
         </div>
     );
