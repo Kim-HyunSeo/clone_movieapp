60bf151f8480d60e2b0bc36f16fa26123101d500
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-04-11 05:04:42 +0900

    Refactor: use createRoot for React 18

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState, useEffect } from "react";
+
 import Button from "./Button";
 import styled from "./App.module.css";
 
 function App() {
+    const [value, setValue] = useState(0);
+    const onClick = () => setValue((prev) => prev + 1);
+    console.log("all");
+    const iRunOnlyOnce = () => {
+        console.log("one");
+    };
+    useEffect(iRunOnlyOnce, []);
+
     return (
         <div>
-            <h1 className={styled.title}>Welcome back!!!</h1>
-            <Button text={"Continue"} />
+            <h1 className={styled.title}>{value}</h1>
+            <Button text={"Click Me!"} onClick={onClick} />
         </div>
     );
 }
